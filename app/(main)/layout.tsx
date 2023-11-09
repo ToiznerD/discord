dc8ba2fee2258220aa8f98f482ac79be2f5f366c
ToiznerD <toiznerd@gmail.com>
2023-11-09 23:53:09 +0200

    refixing auto-zoom for input in mobile

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -1,4 +1,5 @@
 import { NavigationSidebar } from "@/components/navigation/navigation-sidebar";
+import Head from "next/head";
 
 const MainLayout = async ({
     children
@@ -6,14 +7,23 @@ const MainLayout = async ({
         children: React.ReactNode;
 }) => {
     return ( 
-        <div className="h-full">
-            <div className="hidden md:flex h-full w-[72px] z-30 flex-col fixed inset-y-0">
-                <NavigationSidebar />
+        <>
+            <Head>
+                <meta
+                name="viewport"
+                content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no"
+                />
+            </Head>
+            <div className="h-full">
+                <div className="hidden md:flex h-full w-[72px] z-30 flex-col fixed inset-y-0">
+                    <NavigationSidebar />
+                </div>
+                <main className="md:pl-[72px] h-full">
+                    {children}
+                    </main>
             </div>
-            <main className="md:pl-[72px] h-full">
-                {children}
-                </main>
-        </div>
+        </>
+        
      );
 }
  
