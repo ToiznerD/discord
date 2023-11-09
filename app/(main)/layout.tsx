4f65b924260a714545585c88986123af81ead055
ToiznerD <toiznerd@gmail.com>
2023-11-10 00:03:08 +0200

    working on zoom preventing

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -8,12 +8,6 @@ const MainLayout = async ({
 }) => {
     return ( 
         <>
-            <Head>
-                <meta
-                name="viewport"
-                content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no"
-                />
-            </Head>
             <div className="h-full">
                 <div className="hidden md:flex h-full w-[72px] z-30 flex-col fixed inset-y-0">
                     <NavigationSidebar />
