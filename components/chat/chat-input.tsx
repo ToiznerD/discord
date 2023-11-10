56d9731a0546f653feb35739f3b6d2e575aa1511
ToiznerD <toiznerd@gmail.com>
2023-11-10 17:11:28 +0200

    Change chat input buttons positions

--- components/chat/chat-input.tsx
+++ components/chat/chat-input.tsx
@@ -81,27 +81,13 @@ export const ChatInput = ({
                         <FormItem>
                             <FormControl>
                                 <div className="relative p-4 pb-6">
-                                    {isInputEmpty ? (
-                                        <>
-                                        <button
-                                            type="button"
-                                            onClick={() => onOpen("messageFile", {apiUrl, query})}
-                                            className="absolute top-7 left-8 h-[24px] w-[24px] bg-zinc-500 dark:bg-zinc-400 hover:bg-zinc-600 dark:hover:bg-zinc-300 transition rounded-full p-1 flex items-center justify-center"
-                                        >
-                                            <Plus className="text-white dark:text-[#313338]"/>
-                                        </button>
-                                        </>
-                                    ) : (
-                                        <>
-                                        <button
-                                            type="button"
-                                            onClick={form.handleSubmit(onSubmit)}
-                                            className="absolute top-7 left-8 h-[24px] w-[24px] bg-zinc-500 dark:bg-zinc-400 hover:bg-zinc-600 dark:hover:bg-zinc-300 transition rounded-full p-1 flex items-center justify-center"
-                                        >
-                                            <SendHorizonal className="text-white dark:text-[#313338]"/>
-                                        </button>
-                                        </>  
-                                    )}
+                                    <div
+                                        className="absolute top-7 left-8 h-[24px] w-[24px] transition rounded-full p-1 flex items-center justify-center"
+                                    >
+                                        <EmojiPicker
+                                                onChange={(emoji: string) => field.onChange(`${field.value} ${emoji}`)}
+                                        />
+                                    </div>
                                     <Input
                                         disabled={isLoading}
                                         className="px-14 py-6 bg-zinc-200/90 dark:bg-zinc-700/75 border-none border-0 focus-visible:ring-0 focus-visible:ring-offset-0 text-zinc-600 dark:text-zinc-200"
@@ -113,9 +99,27 @@ export const ChatInput = ({
                                         }}
                                     />
                                     <div className="absolute top-7 right-8">
-                                        <EmojiPicker
-                                            onChange={(emoji: string) => field.onChange(`${field.value} ${emoji}`)}
-                                        />
+                                        {isInputEmpty ? (
+                                        <>
+                                        <button
+                                            type="button"
+                                            onClick={() => onOpen("messageFile", {apiUrl, query})}
+                                            className="h-[24px] w-[24px] bg-zinc-500 dark:bg-zinc-400 hover:bg-zinc-600 dark:hover:bg-zinc-300 transition rounded-full p-1 flex items-center justify-center"
+                                        >
+                                            <Plus className="text-white dark:text-[#313338]"/>
+                                        </button>
+                                        </>
+                                        ) : (
+                                            <>
+                                            <button
+                                                type="button"
+                                                onClick={form.handleSubmit(onSubmit)}
+                                                className="h-[24px] w-[24px] bg-zinc-500 dark:bg-zinc-400 hover:bg-zinc-600 dark:hover:bg-zinc-300 transition rounded-full p-1 flex items-center justify-center"
+                                            >
+                                                <SendHorizonal className="text-white dark:text-[#313338]"/>
+                                            </button>
+                                            </>  
+                                        )}
                                     </div>
                                 </div>
                             </FormControl>
